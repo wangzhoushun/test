af92d00ee055b6ee4b291e1d7bf781b95ddfc958
wangzhoushun <wangzhoushun@tuzhanai.com>
2021-01-21 17:36:16 +0800

    测试提交

---
+++ src/aa.java
@@ -0,0 +1,9 @@
+public class aa {
+
+    public static void main(String[] args) {
+        String aa = "2020-12-19 00:00";
+        String[] split = aa.split(" ");
+        System.out.println(split[0]);
+        System.out.println(split[1]);
+    }
+}
